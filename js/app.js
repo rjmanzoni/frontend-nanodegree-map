7e8c99d65d205692338ce4d15190913e80481f97
rmanzoni <rmanzoni@uolinc.com>
2017-06-06 21:28:26 -0300

    show marcador de acordo com a busca

--- js/app.js
+++ js/app.js
@@ -1,3 +1,33 @@
+var map;
+function initMap() {
+   map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: -23.5477279, lng: -46.6436846},
+      zoom: 16
+    });
+
+   for (var i = 0; i < modelLocations.length; i++) {
+          var position = modelLocations[i].location;
+          var title = modelLocations[i].title;
+
+          var marker = new google.maps.Marker({
+            map: map,
+            position: position,
+            title: title,
+            animation: google.maps.Animation.DROP,
+            id: i
+          });
+
+          markers.push(marker);
+
+         // marker.addListener('click', function() {
+          //  populateInfoWindow(this, largeInfowindow);
+          //});
+         // bounds.extend(markers[i].position);
+
+       // map.fitBounds(bounds);
+      }
+}
+
 var modelLocations = [
 	{title: 'Teatro Municipal de Sao Paulo', location: {lat: -23.545235, lng: -46.6386151}, visible:"true"},
 	{title: 'Lanchonete Estadao', location: {lat: -23.5487193, lng: -46.642684}, visible:"true"},
@@ -7,6 +37,8 @@ var modelLocations = [
 	{title: 'Praca Roosevelt', location: {lat: -23.547808, lng: -46.646737}, visible:"true"},
 	{title: 'Edificio Copan', location: {lat: -23.5464774, lng: -46.644516}, visible:"true"}
 ];
+
+var markers = [];
 
 var MarkerViewModel = function(title, location, visible){
 	var self = this;
@@ -21,15 +53,20 @@ var ViewModel = function(){
 	this.filter = ko.observable();
 
 	this.filterList = function(data, event){
-		self.markersList().forEach(function(marker){
-			if(marker.title().toUpperCase().indexOf(data.filter().toUpperCase()) > -1){
-				marker.visible("block");
+
+		for (var i = 0; i < self.markersList().length; i++) {
+			if(self.markersList()[i].title().toUpperCase().indexOf(data.filter().toUpperCase()) > -1){
+				self.markersList()[i].visible("block");
+				markers[i].setMap(map);
 			}
 			else{
-				marker.visible("none");
+				self.markersList()[i].visible("none");
+				markers[i].setMap(null);
 			}
-		});
-		
+		}
+
+
+
 		return true;
 	}
 
@@ -37,36 +74,7 @@ var ViewModel = function(){
 	modelLocations.forEach(function(marker){
 		self.markersList.push(new MarkerViewModel(marker.title, marker.location, marker.visible));
 	});
-	
+
 }
 
 ko.applyBindings(new ViewModel());
-
-var map;
-function initMap() {
-   map = new google.maps.Map(document.getElementById('map'), {
-      center: {lat: -23.5477279, lng: -46.6436846},
-      zoom: 16
-    });
-
-   for (var i = 0; i < modelLocations.length; i++) {
-          var position = modelLocations[i].location;
-          var title = modelLocations[i].title;
-         
-          var marker = new google.maps.Marker({
-            map: map,
-            position: position,
-            title: title,
-            animation: google.maps.Animation.DROP,
-            id: i
-          });
-         // markers.push(marker);
-          
-         // marker.addListener('click', function() {
-          //  populateInfoWindow(this, largeInfowindow);
-          //});
-         // bounds.extend(markers[i].position);
-        
-       // map.fitBounds(bounds);
-      }
-}
