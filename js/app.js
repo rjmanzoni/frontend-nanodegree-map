d10a877333bc1ca7d885b7705b924daf43070676
Renato de Jesus Manzoni <manzoni@MacBook-Pro-de-Renato.local>
2017-06-10 21:53:45 -0300

    ajustes na interface

--- js/app.js
+++ js/app.js
@@ -18,26 +18,24 @@ WikiRestService.prototype.invoke = function(content, callbackSuccess, callBackEr
 }
 
 var map;
+var currentInfoWindow = null;
+var currentMarker = null
 
-//var markersViewModelList = ko.observableArray([]);
-var markersList = [];
+const errorMessage = 'Não foi possivel obter infomação! Possível problema de comunicação com o Wikipidia.';
 
 var modelLocations = [
-	{title: 'Theatro Municipal (São Paulo)', location: {lat: -23.545235, lng: -46.6386151}, visible:"true", wikiList: null},
-	{title: 'Praça da República', location: {lat: -23.543392, lng: -46.642537}, visible:"true", wikiList: null},
-	{title: 'Edificio Itália', location: {lat: -23.545415, lng: -46.643553}, visible:"true", wikiList: null},
-	{title: 'Centro Cultural Banco do Brasil', location: {lat: -23.5474917, lng: -46.6346508}, visible:"true", wikiList: null},
-	{title: 'Praca Roosevelt', location: {lat: -23.547808, lng: -46.646737}, visible:"true", wikiList: null},
-	{title: 'Edificio Copan', location: {lat: -23.5464774, lng: -46.644516}, visible:"true", wikiList: null}
+	{title: 'Theatro Municipal (São Paulo)', location: {lat: -23.545235, lng: -46.6386151}, visible:"true"},
+	{title: 'Praça da República', location: {lat: -23.543392, lng: -46.642537}, visible:"true"},
+	{title: 'Edificio Itália', location: {lat: -23.545415, lng: -46.643553}, visible:"true"},
+	{title: 'Centro Cultural Banco do Brasil', location: {lat: -23.5474917, lng: -46.6346508}, visible:"true"},
+	{title: 'Praca Roosevelt', location: {lat: -23.547808, lng: -46.646737}, visible:"true"},
+	{title: 'Edificio Copan', location: {lat: -23.5464774, lng: -46.644516}, visible:"true"}
 ];
 var mapList = {};
 
 modelLocations.forEach(function(value){
 	mapList[value.title] = {marker: null, infoWindow: null};
 });
-
-var currentInfoWindow = null;
-var currentMarker = null
 
 var wikiRestService = new WikiRestService('https://en.wikipedia.org/w/api.php?action=opensearch&search=$value&format=json&callback=wikiCallback');
 
@@ -65,58 +63,22 @@ function initMap() {
 
         infoWindow.setContent('<div>' + marker.title + '</div>');
 
+        var wiki = function(response){
+        	$('#currentLocation').text(response[2]);
+        }
+
 		marker.addListener('click', showInfoWindowAndMarker(title));
-		//markersList.push(marker);
+		marker.addListener('click', function(title, wiki){
+			return function(){
+				wikiRestService.invoke(title, wiki, function(){$('#currentLocation').text(errorMessage);});
+			}
+		}(title, wiki));
 
 		mapList[title].marker = marker;
 		mapList[title].infoWindow = infoWindow;
-
       }
 }
 
-
-var MarkerModel = function(title, location, visible, marker, infoWindow){
-	var self = this;
-	self.title = ko.observable(title);
-	self.location = ko.observable(location);
-	self.visible = ko.observable(visible);
-	self.marker = marker;
-	self.infoWindow = infoWindow;
-	self.show = animate(marker, infoWindow);
-	self.wikiList =  ko.observable();
-
-	self.fill = function(response){
-		self.wikiList(response[2]);
-	}
-
-	self.err = function(){
-		self.wikiList('Não foi possível recuperar a infomação do wikipedia!');
-	}
-	
-	wikiRestService.invoke(self.title(), self.fill, self.err);
-	
-}
-
-var animate = function(clickedMarker, infoWindow){
-	return function(){
-		//console.log(clickedMarker);
-			if(currentMarker != clickedMarker){
-				currentMarker.setAnimation(null);
-				clickedMarker.setAnimation(google.maps.Animation.BOUNCE);
-				currentMarker = clickedMarker;
-			}else{
-				if(clickedMarker.getAnimation() == null){
-					clickedMarker.setAnimation(google.maps.Animation.BOUNCE);
-				}
-				else{
-					clickedMarker.setAnimation(null);
-				}
-			}
-			currentInfoWindow.close();
-			infoWindow.open(map, clickedMarker);
-			currentInfoWindow = infoWindow;
-		}
-}
 
 var showInfoWindowAndMarker = function(title){
 	return function(){
@@ -138,78 +100,37 @@ var showInfoWindowAndMarker = function(title){
 			infoWindow.open(map, clickedMarker);
 			currentInfoWindow = infoWindow;
 	}
-
-
 }
 
-var ViewModel = function(markersViewModelList){
-	self = this;
-
-	this.filter = ko.observable();
-
-	this.setCurrent = function(location){
-		self.currentLocation(location.wikiList());
-	}
-
-	this.filterList = function(data, event){
-
-		for (var i = 0; i < self.markersList().length; i++) {
-			if(self.markersList()[i].title().toUpperCase().indexOf(data.filter().toUpperCase()) > -1){
-				self.markersList()[i].visible("block");
-				self.markersList()[i].marker.setVisible(true);
-			}
-			else{
-				self.markersList()[i].visible("none");
-				self.markersList()[i].marker.setVisible(false);
-				self.markersList()[i].infoWindow.close();
-			}
-		}
-
-		return true;
-	}
-
-	this.markersList = markersViewModelList;
-	this.currentLocation = ko.observable();
-}
-
-var MarkerModel2 = function(title, location, visible, index){
+var MarkerModel = function(title, location, visible){
 	var self = this;
 	self.title = ko.observable(title);
 	self.location = ko.observable(location);
 	self.visible = ko.observable(visible);
 
-	//self.index = index;
-	self.show = showInfoWindowAndMarker(title);
-
-	self.wikiList =  ko.observable();
-
-	self.fill = function(response){
-		self.wikiList(response[2]);
-	}
-
-	self.err = function(){
-		self.wikiList('Não foi possível recuperar a infomação do wikipedia!');
-	}
-	
-	wikiRestService.invoke(self.title(), self.fill, self.err);
-	
+	self.show = showInfoWindowAndMarker(title);	
 }
 
-var ViewModel2 = function(modelLocations){
+var ViewModel = function(modelLocations){
 	self = this;
 
 	this.filter = ko.observable();
 
 	this.markersList = ko.observableArray();
 
-	this.currentLocation = ko.observable();
-
+	this.currentWikiList = ko.observable();
+	
 	this.setCurrent = function(location){
-		self.currentLocation(location.wikiList());
+
+		var wiki = function(response){
+        		self.currentWikiList(response[2]);
+        	}
+		
+		wikiRestService.invoke(location.title(), wiki, function(){self.currentWikiList(errorMessage)});
 	}
 
 	for (var i = 0; i < modelLocations.length; i++) {
-		this.markersList.push(new MarkerModel2(modelLocations[i].title, modelLocations[i].location, modelLocations[i].visible, i));
+		this.markersList.push(new MarkerModel(modelLocations[i].title, modelLocations[i].location, modelLocations[i].visible));
 
 	}
 
@@ -219,14 +140,11 @@ var ViewModel2 = function(modelLocations){
 			if(self.markersList()[i].title().toUpperCase().indexOf(data.filter().toUpperCase()) > -1){
 				self.markersList()[i].visible("block");
 				mapList[self.markersList()[i].title()].marker.setVisible(true);
-				//self.markersList()[i].marker.setVisible(true);
 			}
 			else{
 				self.markersList()[i].visible("none");
 				mapList[self.markersList()[i].title()].marker.setVisible(false);
-				//self.markersList()[i].marker.setVisible(false);
 				mapList[self.markersList()[i].title()].infoWindow.close();
-				//self.markersList()[i].infoWindow.close();
 			}
 		}
 
@@ -235,22 +153,4 @@ var ViewModel2 = function(modelLocations){
 	
 }
 
-//ko.applyBindings(new ViewModel(markersViewModelList));
-
-ko.applyBindings(new ViewModel2(modelLocations));
-
-
-var test = function(value){
-	value.forEach(function(x){
-		console.log(x);
-	});
-}
-
-var err = function(a,b,c){
-	console.log(a);
-	console.log(b);
-	console.log(c);
-}
-
-wikiRestService.invoke('Centro Cultural Banco do Brasil', test, err);
-
+ko.applyBindings(new ViewModel(modelLocations));
